8129705d1024510a9863504607174716f56323af
prachisanghavi <70756624+prachisanghavi@users.noreply.github.com>
2022-09-28 15:13:44 +0530

    webapi

---
+++ Curdwithapi/backend/WebApplication1/WebApplication1/Context/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿
+using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+
+namespace WebApplication1.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }
+
+    }
+}
---
+++ Curdwithapi/backend/WebApplication1/WebApplication1/Modals/Employee .cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpContact { get; set; }
+        public string EmpEmail { get; set; }
+        public string EmpAddress { get; set; }
+    }
+}
+
